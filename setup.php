bfea9f659cb06b874833f82a1c0790351737537f
yucho123987 <yucho123987@163.com>
2025-04-03 20:39:17 +0800

    汉化 setup.php

--- setup.php
+++ setup.php
@@ -9,35 +9,35 @@ include('config.php');
 
 // test the folder exists, if it doesn't then create it
 if (makeDir($data_directory)) {
-	echo $data_directory . ' folder is ok<br>';
+	echo $data_directory . ' 文件夹正常<br>';
 	
 	// make sure there is an .htaccess file there
 	// if there is not then create one
 	$file = $data_directory .'/.htaccess';
-	if (makehtaccess($file)) { echo '.htaccess file ok in the ' . $data_directory. ' data directory<br>'; } else { echo '.htaccess file exists in the ' . $data_directory .' data directory<br>'; } 	
+	if (makehtaccess($file)) { echo '已成功在数据目录 ' . $data_directory. ' 下创建 htaccess 文件<br>'; } else { echo '.htaccess 文件已存在于数据目录 ' . $data_directory .' 下<br>'; } 	
 } else {
-	echo $data_directory . ' folder does not exist and could not be created<br>';
+	echo $data_directory . ' 不存在且无法被创建<br>';
 }
 
 // based on https://raw.githubusercontent.com/bolt/htaccess_tester/master/htaccess_tester.php
 if (is_readable(__DIR__.'/.htaccess') ) {
 
-    echo "<p>The .htaccess file exists in this folder and is readable to the webserver. These are its contents which should match the repos version: </p>\n<pre>";
+    echo "<p>.htaccess 文件已存在于此目录且对 web 服务器可读。以下是该文件的内容，其应与仓库中的文件相同。</p>\n<pre>";
     echo file_get_contents(__DIR__.'/.htaccess');
     echo "</pre>";
 
 }  else {
 
-    echo "<p><strong>Error:</strong> The .htaccess file does not exist in this folder or it is not readable to the webserver. <br><br>Ensure a .htaccess file is here and matches the repos version. Make sure it is readable to the webserver.</p>";
+    echo "<p><strong>错误：</strong> .htaccess 文件不存在，或其对 web 服务器不可读。<br><br>请确保 .htaccess 存在且和仓库中的内容一致，并使其对 web 服务器可读。</p>";
     die();
 
 }
 
 // https://stackoverflow.com/questions/109188/how-do-i-check-if-a-directory-is-writeable-in-php
 if ( ! is_writable(dirname($data_directory))) {
-    echo dirname($data_directory) . ' must be writable! Please change the permissions to 0744 or 0755<br>';
+    echo dirname($data_directory) . ' 目录必须可写，请将目录权限设置为 0744 或 0755<br>';
 } else {
-    echo $data_directory . ' is writable<br>';
+    echo $data_directory . ' 目录可写<br>';
 }
     
 
